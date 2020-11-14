4c3fc711c3b5c034945c9630b917be1c781bea37
unknown <you@example.comhasan3email@gmail.com>
2020-11-15 01:59:16 +0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as express from "express"
 const app = express();
 import adminv1 from "../routes/admin/v1"
 import userv1 from "../routes/user/v1"
+import * as cors from "cors"
 const port = process.env.PORT || 3000
 
 
@@ -12,7 +13,7 @@ createConnection().then(async connection => {
 app.use(express.json())
 app.use("/v1",userv1)
 app.use("/admin/v1",adminv1)
-
+app.use(cors({ origin: true }));
 
 app.listen(port,()=>{
 console.log(`Running on port ${port}`)
