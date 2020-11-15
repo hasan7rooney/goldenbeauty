97460f1904fdd28bb20a2c5aa4f76a37311476ec
unknown <you@example.comhasan3email@gmail.com>
2020-11-15 23:58:47 +0300

    index

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ const port = process.env.PORT || 3000
 
 createConnection().then(async (connection) => {
 app.use(express.json())
+app.use(cors({ origin: true }));
 app.use("/v1",userv1)
 app.use("/admin/v1",adminv1)
-app.use(cors({ origin: true }));
 
 app.use((req, res, next) => {
     return errRes(res, "404 Not found");
